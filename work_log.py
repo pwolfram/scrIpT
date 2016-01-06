cfe2892880cb059b85ca23aa004e1dc63ec11830
Phillip J. Wolfram <pwolfram@lanl.gov>
2016-01-06 16:10:13 -0700

    Simple script to build a work log

---
+++ work_log.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+import os
+import datetime
+import pickle
+
+def folder_exists(folder, create=True, verbose=True):
+    if not os.path.exists(folder):
+        if create:
+            print 'Creating folder at ', folder
+            os.makedirs(folder)
+        return False
+    else:
+        return True
+
+def load_stored_sets(setlocation):
+    if os.path.isfile(setlocation):
+        with open(setlocation, 'r') as af:
+            setvalues = pickle.load(af)
+    else:
+        setvalues = set()
+    return setvalues
+
+def save_stored_sets(aset, setlocation):
+    print 'saving %s at %s'%(aset, setlocation)
+    with open(setlocation, 'w') as af:
+        pickle.dump(aset, af)
+
+def sanitize_response(response):
+    # separate comma separated items
+    values = response.split(',')
+    # remove spaces in front or back of string
+    values = [av.rstrip() for av in values]
+    values = [av.lstrip() for av in values]
+    return values
+
+def make_log_entry(logfile, timestamp, projname, taskname, entry):
+    with open(logfile, 'a') as lf:
+        if entry == 'START' or entry == 'END':
+            log = entry + '\n'
+        else:
+            log =  'PROJ={' + projname + '}, TASK={' + taskname + '}, LOG={' + entry + '}\n'
+        line = timestamp + '\t '+ log
+        lf.write(line)
+        print 'wrote:\n   %s to\n%s'%(line, logfile)
+
+def undo_log_entry(logfile, timestamp):
+    # remove last line
+    with open(logfile, 'r+') as lf:
+        lines = lf.readlines()
+        lastline = lines[-1]
+        lf.seek(0)
+        for li in lines[:-1]:
+            lf.write(li)
+        lf.truncate()
+
+    undofile = logfile + '.undo'
+    with open(undofile, 'a') as lf:
+        undoline = 'Undo at %s:'%(timestamp) + lastline
+        lf.write(undoline)
+        print 'wrote:\n   %s to\n%s'%(undoline, undofile)
+
+def log_work(database, start, end, undo):
+    now = datetime.datetime.now()
+    yearfolder = database + '/' + "%.4d"%(now.year)
+    monthfolder = yearfolder + '/' + "%.2d"%(now.month)
+    dayfolder = monthfolder + '/' + "%.2d"%(now.day)
+    logfile = dayfolder + '/' + 'daily.log'
+    timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+    
+    for afold in [database, yearfolder, monthfolder, dayfolder]:
+        folder_exists(afold)
+
+    projectfile = database + '/projects.p'
+    projects = load_stored_sets(projectfile)
+
+    taskfile = database + '/tasks.p'
+    tasks = load_stored_sets(taskfile)
+
+    if undo:
+        undo_log_entry(logfile, timestamp)
+    elif start:
+        make_log_entry(logfile, timestamp, '', '', 'START')
+    elif end:
+        make_log_entry(logfile, timestamp, '', '', 'END')
+    else: 
+        print 'Projects = ', sorted(projects)
+        response = raw_input("Please enter project names, e.g., 'proj1, proj2, etc':\n")
+        projname = sanitize_response(response)
+        projects = projects | set(projname)
+        
+        print 'Tasks = ', sorted(tasks)
+        response = raw_input("Please enter task names, e.g., 'analysis, lit_review, misc':\n")
+        taskname = sanitize_response(response)
+        tasks = tasks | set(taskname)
+
+        entry = raw_input("Please log entry:\n")
+        make_log_entry(logfile, timestamp, ','.join(projname), ','.join(taskname), entry)
+
+        save_stored_sets(projects, projectfile)
+        save_stored_sets(tasks, taskfile)
+
+
+if __name__ == "__main__":
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option("-d", "--database_location", dest="database", help="Location for work database", metavar="FOLDER")
+    parser.add_option("-s", "--start", dest="start", help="Starting entry", action='store_true')
+    parser.add_option("-e", "--end", dest="end", help="Ending entry", action='store_true')
+    parser.add_option("-u", "--undo", dest="undo", help="Undo last entry", action='store_true')
+    parser.set_defaults(start=False, end=False, undo=False)
+
+
+    options, args = parser.parse_args()
+    if not options.database:
+        options.database = os.path.expanduser('~') + '/Documents/WorkLogDatabase'
+    
+    print "Using database at " + options.database
+
+    log_work(options.database, options.start, options.end, options.undo)
