89964316fff70f25de9012cc464d4df6457246d3
Phillip Wolfram <pwolfram@blogin1.lcrc.anl.gov>
2017-04-13 09:32:15 -0500

    Adds message to set root-level group and other permissions

--- global_read_permissions.sh
+++ global_read_permissions.sh
@@ -3,5 +3,4 @@
 echo 'setting global read permissions at '$PWD
 find . -type f -exec chmod g+r,o+r {} +
 find . -type d -exec chmod g+rx,o+rx {} +
-
-
+echo 'make sure that there are read / execute permissions for group and others at the top level of the directory, e.g., home'
