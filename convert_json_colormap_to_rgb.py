a8c75a486b3ec361e0292230add53c0a6dc6a401
Phillip J. Wolfram <phillipwolfram@gmail.com>
2019-11-01 09:14:19 -0600

    Capability to grab RGB values from paraview json

---
+++ convert_json_colormap_to_rgb.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import json
+import numpy as np
+import sys
+
+with open(sys.argv[1]) as fin:
+  data = json.load(fin)
+  values = np.asarray(data[0]['RGBPoints']).reshape((16,4))
+  with open(sys.argv[1].split('.')[0] + '.rgb','w') as fout:
+      for line in np.floor(values[:,1:]*255):
+          fout.write('{} {} {}\n'.format(*line))
