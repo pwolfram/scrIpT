1fc25a162f0cdfb0c33e15b6776079377b8ed321
Phillip J. Wolfram <phillipwolfram@gmail.com>
2019-11-01 09:15:35 -0600

    Output file in calling location

--- convert_json_colormap_to_rgb.py
+++ convert_json_colormap_to_rgb.py
@@ -7,6 +7,6 @@ import sys
 with open(sys.argv[1]) as fin:
   data = json.load(fin)
   values = np.asarray(data[0]['RGBPoints']).reshape((16,4))
-  with open(sys.argv[1].split('.')[0] + '.rgb','w') as fout:
+  with open(sys.argv[1].split('.')[0].split('/')[-1] + '.rgb','w') as fout:
       for line in np.floor(values[:,1:]*255):
           fout.write('{} {} {}\n'.format(*line))
